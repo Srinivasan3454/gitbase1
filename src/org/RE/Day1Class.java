02e19b2a8137933a605e553a20a58442aa7a52fa
Srinivasan3454 <Srinivasansankaran25@gmail.com>
2025-06-19 11:21:32 +0530

    Boss

---
+++ src/org/RE/Day1Class.java
@@ -0,0 +1,18 @@
+package org.RE;
+
+public class Day1Class {
+	private void javaCls() {
+		System.out.println("java class started successfully ");
+
+	}
+	private void seliniumCls() {
+		System.out.println("selenium will be started ");
+
+	}
+	public static void main(String[] args) {
+		Day1Class d = new Day1Class();
+		d.javaCls();
+		d.seliniumCls();
+	}
+
+}
